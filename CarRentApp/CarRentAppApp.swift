15633522c3996c8f69b44c09cd8990c7308467f6
A1S1N <nikitosina71ru@gmail.com>
2022-08-20 14:53:38 +0300

    Initial Commit

---
+++ CarRentApp/CarRentAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarRentAppApp.swift
+//  CarRentApp
+//
+//  Created by Никита Александров on 20.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct CarRentAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
